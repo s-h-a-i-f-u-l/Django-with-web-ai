7e88adb02957c9fc25761f12f3f77ecfbee9e13a
s-h-a-i-f-u-l <151225619+s-h-a-i-f-u-l@users.noreply.github.com>
2023-11-23 15:37:59 +0600

    Add files via upload

---
+++ a.py/1.py
@@ -0,0 +1,8 @@
+c = 60
+d = 22
+
+c_float = float(c)
+d_float = float(d)
+
+print("c as float:", c_float)
+print("d as float:", d_float)
---
+++ a.py/2.py
@@ -0,0 +1,11 @@
+info_dict = {
+    "name": "Md. Shaiful Islam",
+    "type": "Student",
+    "personality": "v2",
+    "knowledge_cutoff": "2022-01",
+    "current_date": "2023-11-23"
+}
+
+print("My Info:")
+for key, value in info_dict.items():
+    print(f"{key}: {value}")
